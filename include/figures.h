4a8f0a334d2cd73b3ef7b226311f85b3488773bb
Santana20 <sstfmscontreras@gmail.com>
2021-06-03 16:49:06 -0500

    clear code and add normals in class cube

--- include/figures.h
+++ include/figures.h
@@ -1,76 +1,23 @@
 #ifndef __FIGURES_H__
 #define __FIGURES_H__
 
-class Cube {
+struct RGB {
+	float r, g, b;
+};
 
-	float   width;
-	float   height;
-	float   depth;
-	float*  vertices;
+class Object {
+
+protected:
+	float* vertices;
 	unsigned int* indices;
+	unsigned int  nv;
+	unsigned int  ni;
+	unsigned int  n;
 
 public:
-	Cube(float width=1.0f, float height=1.0f, float depth=1.0f)
-			: width(width), height(height), depth(depth),
-				vertices(new float[16*8]), indices(new unsigned int[6*6]) {
-		float wm = width / 2.0f;
-		float hm = height / 2.0f;
-		float dm = depth / 2.0f;
-		float temp[] = {
-			 // posiciones        colores          texturas
-			-wm,  hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 0
-			 wm,  hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 1
-			-wm, -hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 2
-			 wm, -hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 3
-			-wm,  hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 4
-			 wm,  hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 5
-			-wm, -hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 6
-			 wm, -hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 7
+	Object(unsigned int n, unsigned int m)
+			: vertices(new float[24*n]), indices(new unsigned int[m]), nv(24*n), ni(m), n(n) {}
 
-			-wm,  hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 8
-			 wm,  hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 9
-			-wm, -hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 10
-			 wm, -hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 11
-			-wm,  hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 12
-			 wm,  hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 13
-			-wm, -hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 14
-			 wm, -hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f }; // 15
-			/*-wm,  hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 0
-			 wm,  hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 1
-			-wm, -hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 2
-			 wm, -hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 3
-			-wm,  hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 4
-			 wm,  hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 5
-			-wm, -hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 6
-			 wm, -hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 7
-
-			-wm,  hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 8
-			 wm,  hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 9
-			-wm, -hm,  dm,   1.0f, 0.0, 0.0,   0.0f, 0.0f,  // 10
-			 wm, -hm,  dm,   1.0f, 0.0, 0.0,   1.0f, 0.0f,  // 11
-			-wm,  hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 12
-			 wm,  hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f,  // 13
-			-wm, -hm, -dm,   1.0f, 0.0, 0.0,   0.0f, 1.0f,  // 14
-			 wm, -hm, -dm,   1.0f, 0.0, 0.0,   1.0f, 1.0f}; // 15
-			 */
-		for (unsigned int i = 0; i < 16*8; ++i) {
-			vertices[i] = temp[i];
-		}
-		unsigned int temp2[] = {
-			 0,  1,  2,   1,  2,  3,
-			 8,  9, 12,   9, 12, 13,
-			 1,  5,  3,   3,  5,  7,
-			11, 14, 15,  10, 11, 14,
-			 0,  4,  6,   0,  2,  6,
-			 4,  5,  6,   5,  6,  7};
-		for (unsigned int i = 0; i < 6*6; ++i) {
-			indices[i] = temp2[i];
-		}
-	}
-	~Cube() {
-		delete[] vertices;
-		delete[] indices;
-	}
 	float* getVertices() {
 		return vertices;
 	}
@@ -78,10 +25,81 @@ public:
 		return indices;
 	}
 	unsigned int getVSize() {
-		return 16*8;
+		return nv;
 	}
 	unsigned int getISize() {
-		return 6*6;
+		return ni;
+	}
+	unsigned int len() {
+		return sizeof(float)*n;
+	}
+	void* skip(unsigned int i) {
+		return (void*)(i * sizeof(unsigned int));
+	}
+};
+
+class Cube : public Object {
+	glm::vec3 scale;
+public:
+	Cube(
+		glm::vec3 scale = glm::vec3(1.0f),
+		RGB a = { 1.0f, 1.0f, 1.0f },  // Top
+		RGB b = { 1.0f, 1.0f, 0.0f },  // Bottom
+		RGB c = { 1.0f, 0.0f, 0.0f },  // Front
+		RGB d = { 1.0f, 0.5f, 0.0f },  // Back
+		RGB e = { 0.0f, 1.0f, 0.0f },  // Left
+		RGB f = { 0.0f, 0.0f, 1.0f }   // Right
+	) :
+		Object(11, 6 * 6),
+		scale(scale) {
+		glm::vec3 half = scale * 0.5f;
+
+		float vTemp[] = {
+			//        Positions       |||    Colors    |||       Normals      |||     Textures
+			// Top
+			 half.x,  half.y, -half.z,   a.r, a.g, a.b,    0.0f,  1.0f,  0.0f,   1.0f, 1.0f,  //  0
+			-half.x,  half.y, -half.z,   a.r, a.g, a.b,    0.0f,  1.0f,  0.0f,   0.0f, 1.0f,  //  1
+			-half.x,  half.y,  half.z,   a.r, a.g, a.b,    0.0f,  1.0f,  0.0f,   0.0f, 0.0f,  //  2
+			 half.x,  half.y,  half.z,   a.r, a.g, a.b,    0.0f,  1.0f,  0.0f,   1.0f, 0.0f,  //  3
+			// Bottom
+			 half.x, -half.y,  half.z,   b.r, b.g, b.b,    0.0f, -1.0f,  0.0f,   1.0f, 1.0f,  //  4
+			-half.x, -half.y,  half.z,   b.r, b.g, b.b,    0.0f, -1.0f,  0.0f,   0.0f, 1.0f,  //  5
+			-half.x, -half.y, -half.z,   b.r, b.g, b.b,    0.0f, -1.0f,  0.0f,   0.0f, 0.0f,  //  6
+			 half.x, -half.y, -half.z,   b.r, b.g, b.b,    0.0f, -1.0f,  0.0f,   1.0f, 0.0f,  //  7
+			// Front
+			 half.x,  half.y,  half.z,   c.r, c.g, c.b,    0.0f,  0.0f,  1.0f,   1.0f, 1.0f,  //  8
+			-half.x,  half.y,  half.z,   c.r, c.g, c.b,    0.0f,  0.0f,  1.0f,   0.0f, 1.0f,  //  9
+			-half.x, -half.y,  half.z,   c.r, c.g, c.b,    0.0f,  0.0f,  1.0f,   0.0f, 0.0f,  // 10
+			 half.x, -half.y,  half.z,   c.r, c.g, c.b,    0.0f,  0.0f,  1.0f,   1.0f, 0.0f,  // 11
+			// Back
+			-half.x,  half.y, -half.z,   d.r, d.g, d.b,    0.0f,  0.0f, -1.0f,   1.0f, 1.0f,  // 12
+			 half.x,  half.y, -half.z,   d.r, d.g, d.b,    0.0f,  0.0f, -1.0f,   0.0f, 1.0f,  // 13
+			 half.x, -half.y, -half.z,   d.r, d.g, d.b,    0.0f,  0.0f, -1.0f,   0.0f, 0.0f,  // 14
+			-half.x, -half.y, -half.z,   d.r, d.g, d.b,    0.0f,  0.0f, -1.0f,   1.0f, 0.0f,  // 15
+			// Left
+			-half.x,  half.y,  half.z,   e.r, e.g, e.b,   -1.0f,  0.0f,  0.0f,   1.0f, 1.0f,  // 16
+			-half.x,  half.y, -half.z,   e.r, e.g, e.b,   -1.0f,  0.0f,  0.0f,   0.0f, 1.0f,  // 17
+			-half.x, -half.y, -half.z,   e.r, e.g, e.b,   -1.0f,  0.0f,  0.0f,   0.0f, 0.0f,  // 18
+			-half.x, -half.y,  half.z,   e.r, e.g, e.b,   -1.0f,  0.0f,  0.0f,   1.0f, 0.0f,  // 19
+			// Right
+			 half.x,  half.y, -half.z,   f.r, f.g, f.b,    1.0f,  0.0f,  0.0f,   1.0f, 1.0f,  // 20
+			 half.x,  half.y,  half.z,   f.r, f.g, f.b,    1.0f,  0.0f,  0.0f,   0.0f, 1.0f,  // 21
+			 half.x, -half.y,  half.z,   f.r, f.g, f.b,    1.0f,  0.0f,  0.0f,   0.0f, 0.0f,  // 22
+			 half.x, -half.y, -half.z,   f.r, f.g, f.b,    1.0f,  0.0f,  0.0f,   1.0f, 0.0f,  // 23
+		};
+		unsigned int iTemp[] = {
+			 0,  1,  2,    0,  2,  3,  // Top
+			 4,  5,  6,    4,  6,  7,  // Bottom
+			 8,  9, 10,    8, 10, 11,  // Front
+			12, 13, 14,   12, 14, 15,  // Back
+			16, 17, 18,   16, 18, 19,  // Left
+			20, 21, 22,   20, 22, 23   // Right
+		};
+
+		for (unsigned int i = 0; i < 11 * 24; i++)
+			vertices[i] = vTemp[i];
+		for (unsigned int i = 0; i < 6 * 6; i++)
+			indices[i] = iTemp[i];
 	}
 };
 
--- src/tp_minecraft/main.cpp
+++ src/tp_minecraft/main.cpp
@@ -21,15 +21,6 @@
 #include <mylibs/camera.h>
 #include <figures.h>
 
-// colors of rubik cube
-// blue =   0.0f, 0.0f, 1.0f,
-// white =  1.0f, 1.0f, 1.0f,
-// yellow = 1.0f, 1.0f, 0.0f,
-// green =  0.0f, 0.5f, 0.0f,
-// orange = 1.0f, 0.5f, 0.0f,
-// red =    1.0f, 0.0f, 0.0f,
-
-
 // 2D noise variables
 //int nOutputWidth = 200;
 //int nOutputHeight = 150;
@@ -74,6 +65,10 @@ void PerlinNoise2D(int nWidth, int nHeight, float *fSeed, int nOctaves, float fB
 
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int w, int h) {
+	glViewport(0, 0, w, h);
+}
+
 int width = 800, height = 800;
 
 int main()
@@ -87,7 +82,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	
-	GLFWwindow* window = glfwCreateWindow(width, height, "Maincraft salvaje", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Minecraftt", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -101,6 +96,9 @@ int main()
 	gladLoadGL();
 
 	glViewport(0, 0, width, height);
+
+	// To change the viewport when change the size of windows
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
 	// perlin noise
@@ -136,7 +134,7 @@ int main()
 	medioY /= float(n*n*capas);
 	medioZ /= float(n*n*capas);
 
-
+	// Instance a cube
 	Cube* cube = new Cube();
 
 	// Shader for cube
@@ -149,9 +147,13 @@ int main()
 	// Generates Element Buffer Object and links it to indices
 	EBO EBO(cube->getIndices(), cube->getISize() * sizeof(int));
 	// Links VBO attributes such as coordinates, colors and textures to VAO
-	VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO.LinkAttrib(VBO, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
+	// positions
+	VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, cube->len(), cube->skip(0));
+	// colors
+	VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, cube->len(), cube->skip(3));
+	// tetures
+	VAO.LinkAttrib(VBO, 2, 2, GL_FLOAT, cube->len(), cube->skip(9));
 
 	VAO.Unbind();
 	VBO.Unbind();
@@ -184,34 +186,11 @@ int main()
 
 	// camera
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
-
-// positions for the cube
-/*
-	
-	std::vector<glm::vec3> positions(27);
-	int n = 2, pos = 0;
-	for (int i = -1; i < n; ++i) {
-		for (int j = -1; j < n; ++j) {
-			for (int k = -1; k < n; ++k) {
-				float x = 0.25f;
-				float z = 0.25f;
-				float y = 0.25f;
-				
-				positions[(pos++)] = glm::vec3(i * x, j * y, k * z);
-			}
-		}
-	}
-*/
-
 	glEnable(GL_DEPTH_TEST);
-
-	float prevTime = float(glfwGetTime());
-	int change = 0;
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		float time = float(glfwGetTime());
 		// background color
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		
@@ -226,18 +205,11 @@ int main()
 
 
 		camera.Matrix(shader, "camMatrix");
-/*
-		//stone texture bind
-		if (time - prevTime >= 1.0f) {
-			change = (change + 1) % 3;
-			prevTime = time;
-		}
-		if (change == 0) stoneTexture.Bind();
-		else if (change == 1) grassTexture.Bind();
-		else sandTexture.Bind();
-*/
+
 		VAO.Bind();
 
+
+		// Draw all cubes
 		for (int i = 0; i < int(positions.size()); ++i) {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, positions[i]);
@@ -256,16 +228,13 @@ int main()
 			glDrawElements(GL_TRIANGLES, cube->getISize(), GL_UNSIGNED_INT, 0);
 		}
 		
-
-		// glDrawElements(GL_TRIANGLES, cube->getISize(), GL_UNSIGNED_INT, 0);
 		// Swap the window buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
-
 	// Deletes
+
 	VAO.Delete();
 	VBO.Delete();
 	EBO.Delete();
@@ -280,14 +249,3 @@ int main()
 	glfwTerminate();
 	return 0;
 }
-/*
-//draw all the cubes
-for (int i = 0; i < positions.size(); ++i) {
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::translate(model, positions[i]);
-
-	glUniformMatrix4fv(glGetUniformLocation(Shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
-	glDrawElements(GL_TRIANGLES, sizeof(Indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-}
-*/
